ed3312eace2712e650ef160edd0f862682ca4ee1
DavidVanDrei <vandrei.1@osu.edu>
2023-03-09 22:19:33 -0500

    added auth0

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -4,9 +4,9 @@ import {
   type NextAuthOptions,
   type DefaultSession,
 } from "next-auth";
+import Auth0Provider from "next-auth/providers/auth0";
 import DiscordProvider from "next-auth/providers/discord";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { env } from "~/env.mjs";
 import { prisma } from "~/server/db";
 
 /**
@@ -48,9 +48,14 @@ export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
+      clientId: process.env.DISCORD_CLIENT_ID,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET,
     }),
+    Auth0Provider({
+      clientId: process.env.AUTH0_CLIENT_ID,
+      clientSecret: process.env.AUTH0_CLIENT_SECRET,
+      issuer: process.env.AUTH0_ISSUER
+    })
     /**
      * ...add more providers here.
      *
