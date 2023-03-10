2d605808fd208bc6e7310ac5bbb46e07d0ea6f2a
DavidVanDrei <vandrei.1@osu.edu>
2023-03-09 21:52:32 -0500

    removed auth0

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -5,20 +5,9 @@ import {
   type DefaultSession,
 } from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
-import Auth0Provider from "next-auth/providers/auth0";
-
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { env } from "~/env.mjs";
 import { prisma } from "~/server/db";
-
-declare var process : {
-  env: {
-    NODE_ENV: string,
-    AUTH0_CLIENT_SECRET:string,
-    AUTH0_ISSUER:string,
-    AUTH0_CLIENT_ID:string
-  }
-}
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
@@ -58,13 +47,20 @@ export const authOptions: NextAuthOptions = {
   },
   adapter: PrismaAdapter(prisma),
   providers: [
-    Auth0Provider({
-      // $ExpectType string
-      clientId: process.env.AUTH0_CLIENT_ID,
-      clientSecret: process.env.AUTH0_CLIENT_SECRET,
-      issuer: process.env.AUTH0_ISSUER
-    })
-  ]
+    DiscordProvider({
+      clientId: env.DISCORD_CLIENT_ID,
+      clientSecret: env.DISCORD_CLIENT_SECRET,
+    }),
+    /**
+     * ...add more providers here.
+     *
+     * Most other providers require a bit more work than the Discord provider. For example, the
+     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
+     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
+     *
+     * @see https://next-auth.js.org/providers/github
+     */
+  ],
 };
 
 /**
